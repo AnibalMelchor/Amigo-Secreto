ae0960e64c9985743f7c6dfcf56aa7b05977ed25
Anibal Melchor <melchorcorderoanibal@gmail.com>
2025-01-22 00:19:41 -0600

    Sorteando los nombres y descubriendo el amigo secreto

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
+let indice = 0;
+let lista = document.querySelector('#listaAmigos');
+
 
 function agregarAmigo(){
     // agregamos el valor ingresado por el usuario y guardamos en la variable nombre
@@ -21,11 +24,25 @@ function limpiarInput(){
 }
 
 function actualizarLista(){
-    let lista = document.querySelector('#listaAmigos');
     lista.innerHTML = '';
     for (i = 0; i < amigos.length; i++) {
         let li = document.createElement("li");
         li.textContent = amigos[i];
         lista.appendChild(li);
       } 
+}
+
+function sortearAmigo(){
+    if(amigos.length == 0){
+        alert("Agrega nombres de tus amigos")
+    }else{
+        if(amigos.length == 1){
+            alert("Agrega minimo 2 nombres de tus amigos")
+        }else{
+            lista.innerHTML = '';
+            let indice = Math.floor(Math.random()*amigos.length)+1;
+            let resultado = document.querySelector('#resultado');
+            resultado.innerHTML = `El amigo sorteado es: ${amigos[indice]}`;
+        }
+    }
 }

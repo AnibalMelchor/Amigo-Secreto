105183dbb1a7d26fd3979d57f0d3ed02d92e77c2
Anibal Melchor <melchorcorderoanibal@gmail.com>
2025-01-22 00:37:07 -0600

    Actualizando el README

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
-Desafio de Amigo Secreto
+<h1 align="center"> Juego del Amigo Secreto </h1>
 
--Estado del proyecto en construccion
+*Descripción del Proyecto:
+    El siguiente juego consiste en añadir nombres de varios amigos
+    Al terminar de agregarlos, sortear los nombres y descubrir quien es el amigo secreto
+
+*Estado del proyecto:
+    Finalizado
+
+*Demostración de funciones y aplicaciones:
+    Primero agregamos un nombre a la caja que tien escrito lo siguiente "Escribe un nombre".
+    Posteriormente de agregar un nombre damos click en el boton que dice Añadir.
+    Si no colocamos ningun nombre y seleccionamos el boton, nos mostrara un mensaje diciendo "Por favor, inserte un nombre"
+    Cada que agregemos un nombre se ira mostrando en forma de lista abajo de la caja de texto
+    Despues seleccionamos el boton que dice "Sortear amigo".
+    Si no agregamos ningun nombre nos mostrara un mensaje diciendo "Agrega nombres de tus amigos".
+    De igual manera si solo agregamos un nombre nos mostrara otro mensaje diciendo "Agrega minimo 2 nombres de tus amigos".
+    Ya que si solo tenemos un nombre y lo sorteamos siempre saldra el mismo
+    Si seguimos todos los pasos correctos nos mostrara el mensaje del nombre del amigo secreto sorteado.
+
+*Tercnologias Utilizadas:
+    JavaScript
+
+*Desarrolador:
+    Anibal Melchor Cordero
--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let amigos = [];
 let indice = 0;
 let lista = document.querySelector('#listaAmigos');
 
-
+//funcion para agregar el amigo a la lista
 function agregarAmigo(){
     // agregamos el valor ingresado por el usuario y guardamos en la variable nombre
     let nombre = document.getElementById("amigo").value;
@@ -12,35 +12,47 @@ function agregarAmigo(){
         //Si no coloca nigun nombre lanzamos una alerta de que ingrese un nombre
         alert("Por favor, inserte un nombre");
     }else{
+        // agregamos los nombres colocados en la lista
         amigos.push(nombre);
         limpiarInput();
         actualizarLista();
     }
 }
 
+//funcion para limpiar el campo donde escribimos el nombre
 function limpiarInput(){
     let campo = document.querySelector('#amigo');
     campo.value = '';
 }
 
+//funcion para actiualizar la lista
 function actualizarLista(){
     lista.innerHTML = '';
+    //iteramos o recorremos la lista y mostramos los nombres en una lista en el html
     for (i = 0; i < amigos.length; i++) {
+        //agregamos nombres a la lista del html
         let li = document.createElement("li");
         li.textContent = amigos[i];
         lista.appendChild(li);
       } 
 }
 
+//funcion para sortear el amigo
 function sortearAmigo(){
+    //validamos que la lista tenga nombres de los amigos al momento de sortear
     if(amigos.length == 0){
+        //en caso de que no tenga nombres mostramos una alerta
         alert("Agrega nombres de tus amigos")
     }else{
+        // validamos que la lista tenga mas de un nombre, ya que si solo sorteamos un nombre siempre saldra el mismo
         if(amigos.length == 1){
             alert("Agrega minimo 2 nombres de tus amigos")
         }else{
+            // generamos un numero aleatorio entre la cantidad de nombres añadidos en la lista
             lista.innerHTML = '';
             let indice = Math.floor(Math.random()*amigos.length)+1;
+
+            // mostramos el nombre del amigo secreto en el html
             let resultado = document.querySelector('#resultado');
             resultado.innerHTML = `El amigo sorteado es: ${amigos[indice]}`;
         }
